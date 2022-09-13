6af2e14b2c7de8ff32c8fa895d3bc917080b9e50
Zsolt Szunyoghy <zsolt.szunyoghy@freemail.hu>
2022-09-13 23:10:56 +0200

    bodyToFlux() - line by line

--- src/main/java/com/example/webclienttest/WebclienttestApplication.java
+++ src/main/java/com/example/webclienttest/WebclienttestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -13,11 +14,11 @@ public class WebclienttestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebclienttestApplication.class, args);
 
-		Mono<String> result = WebClient.create().get().uri("http://google.com").retrieve()
+		Flux<String> result = WebClient.create().get().uri("http://google.com").retrieve()
 				// .onStatus(status -> status.value() == 301,
 				// clientResponse -> Mono.empty())
 				// .toEntity(String.class);// .block();
-				.bodyToMono(String.class);
+				.bodyToFlux(String.class);
 
 		result.subscribe(s -> {
 			System.out.println("<<" + s + ">>");
