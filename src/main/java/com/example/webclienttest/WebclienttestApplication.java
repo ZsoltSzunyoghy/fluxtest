695ed6676321c54da50c432f75f74fb0d7e57aba
Zsolt Szunyoghy <zsolt.szunyoghy@freemail.hu>
2022-09-13 23:10:13 +0200

    bodyToMono()

--- src/main/java/com/example/webclienttest/WebclienttestApplication.java
+++ src/main/java/com/example/webclienttest/WebclienttestApplication.java
@@ -13,15 +13,14 @@ public class WebclienttestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebclienttestApplication.class, args);
 
-		Mono<ResponseEntity<String>> result = WebClient.create().get().uri("http://google.com").retrieve()
+		Mono<String> result = WebClient.create().get().uri("http://google.com").retrieve()
 				// .onStatus(status -> status.value() == 301,
 				// clientResponse -> Mono.empty())
-				.toEntity(String.class);// .block();
-		// .bodyToMono(String.class)
+				// .toEntity(String.class);// .block();
+				.bodyToMono(String.class);
 
 		result.subscribe(s -> {
-			System.out.println(s.getStatusCode().value());
-			System.out.println(s.getBody());
+			System.out.println("<<" + s + ">>");
 		});
 
 		// System.out.println("status code: " + result.getStatusCodeValue());
