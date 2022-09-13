2c8be13471c387e850b062ac12581f8a9751d372
Zsolt Szunyoghy <zsolt.szunyoghy@freemail.hu>
2022-09-13 22:42:35 +0200

    do the same with subscribe

--- src/main/java/com/example/webclienttest/WebclienttestApplication.java
+++ src/main/java/com/example/webclienttest/WebclienttestApplication.java
@@ -13,13 +13,19 @@ public class WebclienttestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebclienttestApplication.class, args);
 
-		ResponseEntity<String> result = WebClient.create().get().uri("http://google.com").retrieve()
+		Mono<ResponseEntity<String>> result = WebClient.create().get().uri("http://google.com").retrieve()
 				// .onStatus(status -> status.value() == 301,
 				// clientResponse -> Mono.empty())
-				.toEntity(String.class).block();
+				.toEntity(String.class);// .block();
+		// .bodyToMono(String.class)
 
-		System.out.println("status code: " + result.getStatusCodeValue());
-		System.out.println("response body: " + result.getBody());
+		result.subscribe(s -> {
+			System.out.println(s.getStatusCode().value());
+			System.out.println(s.getBody());
+		});
+
+		// System.out.println("status code: " + result.getStatusCodeValue());
+		// System.out.println("response body: " + result.getBody());
 	}
 
 }
